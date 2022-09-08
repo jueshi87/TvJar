4eae5941d3dd2699f7d9634861ecb9b3a1d4ac61
jueshi87 <71540237+jueshi87@users.noreply.github.com>
2022-09-08 13:36:20 +0800

    Update AES.java

--- app/src/main/java/com/github/catvod/utils/AES.java
+++ app/src/main/java/com/github/catvod/utils/AES.java
@@ -30,7 +30,20 @@ public class AES {
         return null;
     }
 
-
+public String encrypt(String value) {
+        try {
+            byte[] key = KEY_AES.getBytes("UTF-8");
+            byte[] ivs = KEY_AES.getBytes("UTF-8");
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+            AlgorithmParameterSpec paramSpec = new IvParameterSpec(ivs);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, paramSpec);
+            return Base64.encodeToString(cipher.doFinal(value.getBytes("UTF-8")), Base64.DEFAULT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
     public static Cipher GetCipher(int opmode, String key) {
         try {
             DESKeySpec dks = new DESKeySpec(key.getBytes());
